4e5629ad5b8ea1f4b6afce7d6d7bf0d8cf62b4ce
sigmaroles <sohhom@gmail.com>
2020-12-17 13:02:26 +0530

    huh. QUEST is perhaps inferior to UML..

--- questDemo_copy.m
+++ questDemo_copy.m
@@ -1,31 +1,29 @@
 clear all;
 close all;
 
-%animate=input('Do you want so see a live animated plot of the shrinking pdf? (y/n) [y]:','s');
-%animate=~streq(lower(animate),'n');
-
 % user inputs
-animate = 'y';
-tActual=-10;
-tGuess=-8;
-tGuessSd=8;
+animate = 0; % logical, 1 or 0
+tActual = -12.3;
+tGuess = -10;
+tGuessSd = 30;
 
-% predefined params
-pThreshold=0.82;
-beta=3.5;delta=0.01;gamma=0.5;
-
-
-q=QuestCreate(tGuess,tGuessSd,pThreshold,beta,delta,gamma);
+% predefined params and vars
+pThreshold=0.99;
+pbeta=5.5;
+pdelta=0.005;
+pgamma=0.8;
 q.normalizePdf=1; % This adds a few ms per call to QuestUpdate, but otherwise the pdf will underflow after about 1000 trials.
-
 trialsDesired=200;
 wrongRight={'wrong','right'};
 
+% init
+qObject=QuestCreate(tGuess,tGuessSd,pThreshold,pbeta,pdelta,pgamma);
 
+% simulate trials
 for k=1:trialsDesired
 	% Get recommended level.  Choose your favorite algorithm.
-	%tTest=QuestQuantile(q);	% Recommended by Pelli (1987), and still our favorite.
-	 	tTest=QuestMean(q);		% Recommended by King-Smith et al. (1994)
+	tTest=QuestQuantile(qObject);	% Recommended by Pelli (1987), and still our favorite.
+   %tTest=QuestMean(q);		% Recommended by King-Smith et al. (1994)
 	% 	tTest=QuestMode(q);		% Recommended by Watson & Pelli (1983)
 	
 	% We are free to test any intensity we like, not necessarily what Quest suggested.
@@ -34,17 +32,17 @@ for k=1:trialsDesired
 	% Simulate a trial
 	if animate
       figure(1);
-      response=QuestSimulate(q,tTest,tActual,2);
+      response=QuestSimulate(qObject,tTest,tActual,2);
       title('Actual psychometric function, and the points tested.')
       xl=xlim;
    else
-      response=QuestSimulate(q,tTest,tActual);
+      response=QuestSimulate(qObject,tTest,tActual);
    end
 
- 	fprintf('Trial %3d at %5.2f is %s\n',k,tTest,char(wrongRight(response+1)));
+ 	%fprintf('Trial %3d at %5.2f is %s\n',k,tTest,char(wrongRight(response+1)));
 		
 	% Update the pdf
-	q=QuestUpdate(q,tTest,response); % Add the new datum (actual test intensity and observer response) to the database.
+	q=QuestUpdate(qObject,tTest,response); % Add the new datum (actual test intensity and observer response) to the database.
    if animate
       figure(2)
       plot(q.x+q.tGuess,q.pdf)
@@ -59,11 +57,11 @@ end
 t=QuestMean(q);		% Recommended by Pelli (1989) and King-Smith et al. (1994). Still our favorite.
 sd=QuestSd(q);
 
-fprintf('Final threshold estimate (mean+-sd) is %.2f +- %.2f\n',t,sd);
-% t=QuestMode(q);	% Similar and preferable to the maximum likelihood recommended by Watson & Pelli (1983).
-% fprintf('Mode threshold estimate is %4.2f\n',t);
-fprintf('\nYou set the true threshold to %.2f.\n',tActual);
-fprintf('Quest knew only your guess: %.2f +- %.2f.\n',tGuess,tGuessSd);
+fprintf('\nThreshold estimate : %.4f +- %.4f\n',t,sd);
+t=QuestMode(q);	% Similar and preferable to the maximum likelihood recommended by Watson & Pelli (1983).
+fprintf('Mode threshold estimate is %.4f\n',t);
+fprintf('\nTrue threshold : %.4f.\n',tActual);
+fprintf('Guess threshold init: %.4f +- %.4f \n\n',tGuess,tGuessSd);
 
 if animate
    figure(1)
